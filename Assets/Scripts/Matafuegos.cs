4e3cb8382a9e9db23397f1d4f123f618df2bfb24
MeliKeni <melinakenigsberg@gmail.com>
2025-08-05 22:39:34 -0300

    Mas avance en la task1

---
+++ Assets/Scripts/Matafuegos.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Matafuegos : MonoBehaviour
+{
+    public ParticleSystem particulas;
+    private bool dentroDelTriggerMano = false;
+
+    void Start()
+    {
+        particulas.Stop();
+    }
+
+    void Update()
+    {
+        if (dentroDelTriggerMano)
+        {
+            if (Input.GetKey(KeyCode.G))
+            {
+                if (!particulas.isPlaying)
+                    particulas.Play();
+            }
+            else
+            {
+                if (particulas.isPlaying)
+                    particulas.Stop();
+            }
+        }
+        else
+        {
+            if (particulas.isPlaying)
+                particulas.Stop();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Sphere")
+        {
+            dentroDelTriggerMano = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Sphere")
+        {
+            dentroDelTriggerMano = false;
+        }
+    }
+}
